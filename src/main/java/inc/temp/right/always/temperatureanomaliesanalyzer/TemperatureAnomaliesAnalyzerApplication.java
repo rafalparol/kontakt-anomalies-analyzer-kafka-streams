bde2762f6490ea90e49e53d64ed5de6068d0b581
rafal-parol <rafal.parol@gmail.com>
2024-02-24 23:08:33 +0100

    First commit.

---
+++ src/main/java/inc/temp/right/always/temperatureanomaliesanalyzer/TemperatureAnomaliesAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package inc.temp.right.always.temperatureanomaliesanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureAnomaliesAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureAnomaliesAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inc/temp/right/always/temperatureanomaliesanalyzer/TemperatureAnomaliesAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.temp.right.always.temperatureanomaliesanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureAnomaliesAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
